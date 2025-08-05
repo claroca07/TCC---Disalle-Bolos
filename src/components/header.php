f014c38c405c76a09a852fbf121d0e270224ff01
claroca07 <anaclara123.limaandre@gmail.com>
2025-08-04 22:54:21 -0300

    Menu de navegação responsivo

--- src/components/header.php
+++ src/components/header.php
@@ -1,33 +1,50 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
+
 <body>
-    
-    <header class='col-lg-6 d-flex'>
-        
-            <img id='logo' class='col-md-3 col-lg-6 m-auto mt-3' src="../assets/img/nav_button.png" height='40px' width='40px'>
 
-            
-            <h1 class='col-md-5 col-lg-2 fs-4 m-auto mt-4'>Disalle Bolos</h1>
-
-                
-
-            <button class='btn btn-md btn-lg col-md-3 col-lg-6 m-auto '>
-                <img id='nav_button' src="../assets/img/nav_button.png" height="35px" width="35px">
-            </button>
-
-        
-
+    <header class="d-flex justify-content-between p-4 px-3 align-items-center">
+        <h1 class="fs-2 mb-0">Disalle Bolos</h1>
+        <button class="btn p-0 d-md-none me-3">
+            <img id="nav_button" src="../assets/img/layout/menu-hamburguer.png" height="35" width="35" alt="Menu">
+        </button>
+        <div class="d-none d-md-flex justify-content-around me-3">
+            <ul class="d-flex mb-0 align-items-center">
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="#">Bolos</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="#">Doces</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="#">Kit Festa</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="#">Salgados</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="#">Mini-Lanche</a>
+                </li>
+                <li class="nav-item mx-3">
+                    <a class="nav-link" href="#">
+                        <button class="btn p-0">
+                            <img id="buy_button" src="../assets/img/layout/carrinho de compras.png" height="35" width="35" alt="Menu">
+                        </button>
+                    </a>
+                </li>
+            </ul>
+            </nav>
+        </div>
     </header>
 
 
-
-
-
 </body>
+
 </html>
