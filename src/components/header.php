65c5168f15e3a61ecbef60ea664df5258193d93a
Dali^^ <161710415+D4liszzCodes@users.noreply.github.com>
2025-08-04 21:41:34 -0300

    Add files via upload

--- src/components/header.php
+++ src/components/header.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
+</head>
+<body>
+    
+    <header class='col-lg-6 d-flex'>
+        
+            <img id='logo' class='col-md-3 col-lg-6 m-auto mt-3' src="../assets/img/nav_button.png" height='40px' width='40px'>
+
+            
+            <h1 class='col-md-5 col-lg-2 fs-4 m-auto mt-4'>Disalle Bolos</h1>
+
+                
+
+            <button class='btn btn-md btn-lg col-md-3 col-lg-6 m-auto '>
+                <img id='nav_button' src="../assets/img/nav_button.png" height="35px" width="35px">
+            </button>
+
+        
+
+    </header>
+
+
+
+
+
+</body>
+</html>
