2730c2f79878f5884baf281402fadae100596acf
claroca07 <anaclara123.limaandre@gmail.com>
2025-08-10 18:57:38 -0300

    Consertando caminhos

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
@@ -15,25 +15,25 @@
             <h1>Contatos</h1>
             <li>
                 <a href="https://maps.app.goo.gl/GvdpmTi86pXLCoWc8">
-                    <img src="../assets/img/layout/logo_localizacao.png" height="40" width="30" alt="Localização">
+                    <img src="./assets/img/layout/logo_localizacao.png" height="40" width="30" alt="Localização">
                     <h3>Av.João Quevedo de Miranda, 300. Enxovia- Tatuí, SP</h3>
                 </a>
             </li>
             <li>
                 <a href="https://api.whatsapp.com/send?phone=5515997220672">
-                    <img src="../assets/img/layout/logo_whatsapp.png" height="50" width="50" alt="Whatsapp">
+                    <img src="./assets/img/layout/logo_whatsapp.png" height="50" width="50" alt="Whatsapp">
                     <h3>(15)99722-0672</h3>
                 </a>
             </li>
             <li>
                 <a href="https://www.instagram.com/disallebolos">
-                    <img src="../assets/img/layout/logo_instagram.png" height="35" width="35" alt="Instagram">
+                    <img src="./assets/img/layout/logo_instagram.png" height="35" width="35" alt="Instagram">
                     <h3>@disallebolos</h3>
                 </a> 
             </li>
             <li>
                 <a href="https://www.facebook.com/709863109093626?ref=_xav_ig_profile_page_web">
-                    <img src="../assets/img/layout/logo_facebook.png" height="35" width="35" alt="Facebook">
+                    <img src="./assets/img/layout/logo_facebook.png" height="35" width="35" alt="Facebook">
                     <h3>Disalle Bolos Tatuí</h3>
                 </a> 
             </li>
--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
@@ -15,7 +15,7 @@
             <h1 class="fs-1 mb-0 ms-4">DISALLE BOLOS</h1>
         </a>
         <button id="btn-menu" class="btn p-0 d-md-none me-3">
-            <img id="nav_button" src="../assets/img/layout/menu-hamburguer.png" height="35" width="35" alt="Menu">
+            <img id="nav_button" src="./assets/img/layout/menu-hamburguer.png" height="35" width="35" alt="Menu">
         </button>
 
         <div class="menu-desktop d-none d-md-flex justify-content-around">
@@ -38,7 +38,7 @@
                 <li class="nav-item mx-2">
                     <a class="nav-link" href="../pages/carrinho.php">
                         <button class="btn p-0">
-                            <img id="buy_button" src="../assets/img/layout/carrinho de compras.png" height="35" width="35" alt="Carrinho">
+                            <img id="buy_button" src="./assets/img/layout/carrinho de compras.png" height="35" width="35" alt="Carrinho">
                         </button>
                     </a>
                 </li>
@@ -63,14 +63,14 @@
                 <a class="nav-link" href="../pages/mini_lanches.php">MINI-LANCHE</a>
             </li>
             <li class="nav-item ms-5">
-                <a class="nav-link" href="../pages/carrinho.php">
+                <a class="nav-link" href="./pages/carrinho.php">
                     <button class="btn p-0">
-                        <img id="buy_button" src="../assets/img/layout/carrinho de compras.png" height="35" width="35" alt="Carrinho">
+                        <img id="buy_button" src="./assets/img/layout/carrinho de compras.png" height="35" width="35" alt="Carrinho">
                     </button>
                 </a>
             </li>
     </div>
 </body>
-<script src="../assets/js/main.js"></script>
+<script src="./assets/js/main.js"></script>
 
 </html>
